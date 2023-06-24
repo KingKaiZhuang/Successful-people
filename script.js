7e5077a6d2fd76a409fb2b5147819a5d7255098d
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-06-25 00:02:32 +0800

    RWD

--- script.js
+++ script.js
@@ -14,32 +14,52 @@ for(collapseBtn of collapseBtns){
     }
 }  
 /* Slider */
-let imgs = document.querySelectorAll('.slider-item');
-let total = imgs.length;
-let currentIndex = 0;
-let next = document.querySelector('.slider-next');
-let prev = document.querySelector('.slider-prev');
+// let imgs = document.querySelectorAll('.slider-item');
+// let total = imgs.length;
+// let currentIndex = 0;
+// let next = document.querySelector('.slider-next');
+// let prev = document.querySelector('.slider-prev');
 
-next.onclick = () => {
-    slider();
-}
-prev.onclick = () => {
-    slider('prev');
-}
+// next.onclick = () => {
+//     slider('next');
+// }
+// prev.onclick = () => {
+//     slider('prev');
+// }
 
-function slider(direction){
-    console.log(currentIndex);
-    // if(currentIndex == total - 1){
-    //     currentIndex = 0;
-    // }else{
-    //     currentIndex++;
-    // }
-    if(direction === 'prev'){
-        currentIndex = currentIndex === 0 ? total : currentIndex;
-        currentIndex = (currentIndex - 1) % total;
+// function slider(direction){
+//     console.log(currentIndex);
+//     // if(currentIndex == total - 1){
+//     //     currentIndex = 0;
+//     // }else{
+//     //     currentIndex++;
+//     // }
+//     if(direction === 'prev'){
+//         currentIndex = currentIndex === 0 ? total : currentIndex;
+//         currentIndex = (currentIndex - 1) % total;
+//     }else{
+//         currentIndex = (currentIndex + 1) % total;
+//     }
+//     document.querySelector('.slider-item.active').classList.remove('active');
+//     imgs[currentIndex].classList.add('active');
+// }
+
+// 取得視窗寬度
+
+let bodyWidth = document.body.clientWidth;
+let htmlWidth = document.documentElement.clientWidth;
+console.log(bodyWidth);
+console.log(htmlWidth);
+
+window.onresize = e => {
+    let widowWidth = window.innerWidth;
+    let menu = document.querySelector('.menu-group');
+    console.log(widowWidth);
+    if(widowWidth > 1200){
+        menu.style.height = 'initial';
+        menu.style.overflow = 'initial';
     }else{
-        currentIndex = (currentIndex + 1) % total;
+        menu.style.height = 0;
+        menu.style.overflow = 'hidden';
     }
-    document.querySelector('.slider-item.active').classList.remove('active');
-    imgs[currentIndex].classList.add('active');
 }
