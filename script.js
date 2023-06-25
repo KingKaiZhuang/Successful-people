0f78e15ea380adca24164c5869be219a0bdb3d91
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-06-25 21:57:17 +0800

    RWD

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let collapseBtns = document.querySelectorAll('.collapse-btn');
 for(collapseBtn of collapseBtns){
     collapseBtn.onclick = e => {
         let panel = e.target.nextElementSibling;
+        e.target.classList.toggle('active');
         panel.classList.toggle('active');
         console.log(panel.style.height);
         if(panel.style.height){
